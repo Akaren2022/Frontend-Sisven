24b21dcfb1f95c0b82c6fb11b8399bcff26ead78
Akaren2022 <karenaitana21@gmail.com>
2024-05-22 20:05:24 -0500

    Creacion de las vistas para clientes y para categorias con sus rutas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link to="/clientes">Clientes</router-link> |
+    <router-link to="/categorias">Categorias</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,49 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import Cliente from '../views/Clientes.vue'
+import NuevoCliente from '../components/clientes/NuevoCliente.vue'
+import EditarCliente from '../components/clientes/EditarCliente.vue'
+
+import ListarCategorias from '../views/Categorias.vue'
+import NuevaCategoria from '../components/categorias/NuevaCategoria.vue'
+import EditarCategoria from '../components/categorias/EditarCategoria.vue'
+
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/clientes',
+    name: 'Cliente',
+    component: Cliente
+  },
+  {
+    path: '/clientes/nuevo',
+    name: 'NuevoCliente',
+    component: NuevoCliente
+  },
+  {
+    path: '/editar-cliente/:id',
+    name: 'EditarCliente',
+    component: EditarCliente
+  },
+  {
+    path: '/categorias',
+    name: 'ListarCategorias',
+    component: ListarCategorias
+  },
+  {
+    path: '/categorias/nuevo',
+    name: 'NuevaCategoria',
+    component: NuevaCategoria
+  },
+  {
+    path: '/editar-Categoria/:id',
+    name: 'EditarCategoria',
+    component: EditarCategoria
   },
   {
     path: '/about',
